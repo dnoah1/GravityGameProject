1ecf42d65af5ce1f1c755bef1ebbd7cd167b0a55
hannah metzger <hannahmetzger@800G2-1767.DisStu.local>
2020-02-18 10:55:16 +0100

    coin stuff

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    SpriteRenderer sr;
+    public int coinScore;
+    public GameObject scoreKeeper;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>(); 
+    }
+    
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log(col.gameObject.name + " : " + gameObject.name + " : " + Time.time);
+        if (col.CompareTag("coin"))
+        {
+            
+            Debug.Log("collided with coin");
+            coinScore++;
+            scoreKeeper.GetComponent<Timer>().addScore(coinScore);
+
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
